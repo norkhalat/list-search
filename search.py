fcfd7b9e446d088b7be1deff57eee885ec132917
Samvel Khalatyan <sn.khalatyan@gmail.com>
2018-02-25 15:57:54 -0500

    search: add function skeleton

---
+++ search.py
@@ -0,0 +1,8 @@
+def index(number, numbers):
+    ''' return the index of the number in the list of integers numbers
+
+    The function returns -1 if the number is not present in the numbers,
+    or the list is empty.
+    '''
+
+    return -1
